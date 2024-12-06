98d0fedda0e16ad24cd8641aba44d296c9e41ef1
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-06 13:54:41 +0100

    AddJobPage completed

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -8,7 +8,9 @@ const JobListing = ({ job }) => {
   let description = job.description;
 
   if (!showFullDescription) {
-    description = description.substring(0, 90) + "...";
+    //description = description.substring(0, 90) + "...";
+    //safer way
+    description = description ? description.substring(0, 90) + "..." : "";
   }
 
   return (
@@ -22,7 +24,7 @@ const JobListing = ({ job }) => {
         <div className="mb-5">{description}</div>
 
         <button
-          onClick={() => setShowFullDescription((prev) => !prev)}
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
           className="text-indigo-500 mb-5 hover:text-indigo-600"
         >
           {showFullDescription ? "Less" : "More"}
@@ -34,7 +36,7 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <FaMapMarker className="text-lg inline mr-1 mb-1" />
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <Link
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,5 +1,5 @@
 import { useParams, useLoaderData, Link } from "react-router-dom";
-import { FaArrowCircleLeft } from "react-icons/fa";
+import { FaArrowCircleLeft, FaMapMarker } from "react-icons/fa";
 
 const JobPage = () => {
   const { id } = useParams();
@@ -44,7 +44,8 @@ const JobPage = () => {
                 <h1 className="text-3xl font-bold mb-4">{job.title}</h1>
                 <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
                   <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                  <p className="text-orange-700">Boston, MA</p>
+                  <FaMapMarker className="text-lg text-orange-700 mr-2" />
+                  <p className="text-orange-700">{job.location}</p>
                 </div>
               </div>
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
@@ -78,7 +79,7 @@ const JobPage = () => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to="/add-job"
+                  to={`/jobs/edit/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
