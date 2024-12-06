0309859750215cd128dd4c4f507449e71da8af59
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-06 14:18:10 +0100

    DeleteJob and Toastr added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,12 @@
 import Navbar from "../components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <Outlet />
     </>
