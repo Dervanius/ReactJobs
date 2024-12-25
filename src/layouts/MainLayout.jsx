9b7e4192d8003cacfd0254b7cf6de5d1c964c939
Branko Ristanovic <bristanovic@hotmail.com>
2024-12-25 12:42:11 +0100

    Toastr customized

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,28 @@
-import Navbar from "../components/Navbar";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router-dom";
+import { ToastContainer, Flip } from "react-toastify";
+import { FaArrowCircleUp } from "react-icons/fa";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      <ToastContainer />
+      <ToastContainer
+        icon={FaArrowCircleUp}
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        // rtl={false}
+        // pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+      />
     </>
   );
 };
